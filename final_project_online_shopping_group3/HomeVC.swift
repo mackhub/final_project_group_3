40743677b5bd50df9b772204c4e346915d18aa19
Default <flotazani@atc203b-m29.fhda.edu>
2016-11-23 12:43:54 -0800

    started working on login and sing up process

---
+++ final_project_online_shopping_group3/HomeVC.swift
@@ -0,0 +1,55 @@
+//
+//  HomeVC.swift
+//  final_project_online_shopping_group3
+//
+//  Created by Andrei  Konovalov on 11/23/16.
+//  Copyright © 2016 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    override func viewDidAppear(animated: Bool) {
+//        self.performSegueWithIdentifier("goto_login", sender: self)
+//    }
+    
+    @IBAction func logoutTapped(sender: UIButton) {
+        self.performSegueWithIdentifier("goto_login", sender: self)
+
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else {
+           // self.usernameLabel.text = prefs.valueForKey("USERNAME") as NSString
+        }
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ final_project_online_shopping_group3/LoginVC.swift
@@ -0,0 +1,39 @@
+//
+//  LoginVC.swift
+//  final_project_online_shopping_group3
+//
+//  Created by Andrei  Konovalov on 11/23/16.
+//  Copyright © 2016 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet var txtPassword: UITextField!
+    @IBOutlet var txtUsername: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
