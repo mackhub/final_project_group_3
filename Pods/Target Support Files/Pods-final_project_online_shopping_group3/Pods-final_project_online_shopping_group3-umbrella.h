efaaba55c57cfe2f1c32cd5bc585ab8e00ff507a
Mack Hashemi <mack@hashemi.com>
2016-11-28 17:11:14 -0800

    New Files plus app changes for Firebase / Pods

---
+++ Pods/Target Support Files/Pods-final_project_online_shopping_group3/Pods-final_project_online_shopping_group3-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_final_project_online_shopping_group3VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_final_project_online_shopping_group3VersionString[];
+
